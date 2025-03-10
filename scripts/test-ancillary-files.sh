3a245c30ef12872951b6af8f2f316a1a7513ffe6
Colin Clark <colin.clark@cclark.uk>
2025-03-10 10:38:09 +0000

    test 2

--- scripts/test-ancillary-files.sh
+++ scripts/test-ancillary-files.sh
@@ -370,8 +370,10 @@ exit_status = 0
 }
 
 NR == FNR{a[$0] = "";next} !($0 in a) {
-	exit_status = 1
-	print "Bash completions - Option error: " $0
+	if ($0 != "--debug=" && $0 != "--grep=") {
+		exit_status = 1
+		print "Bash completions - Option error: " $0
+		}
 	}
 
 END {
